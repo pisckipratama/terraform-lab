10b71d1d200348b969a5b949ed713df1982e05f7
Piscki <pisckipratama@gmail.com>
2022-05-12 17:32:10 +0700

    add project 5

---
+++ 05-ec2-instances/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    "Name" = "Default VPC"
+  }
+}
+
+// HTTP & HTTPS Server -> 80 TCP, 443 TCP, SSH ->22 TCP, CIDR
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "name" = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "test-terraform-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to Pisckitamas Site - Virtual server is ${self.public_dns} | sudo tee /var/www/html/index.html",
+    ]
+  }
+}
---
+++ 05-ec2-instances/variables.tf
@@ -0,0 +1,3 @@
+variable "aws_key_pair" {
+  default = "./test-terraform-ec2.pem"
+}
